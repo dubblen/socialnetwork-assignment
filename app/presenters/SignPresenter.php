6db4ebdc3012fae8fd8c51764a7775db21c6d914
Nathan Němec <regdit47@gmail.com>
2018-03-09 12:59:54 +0100

    User manager, sign in/up forms

---
+++ app/presenters/SignPresenter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+use App\Forms;
+
+class SignPresenter extends Nette\Application\UI\Presenter
+{
+	/** @var Forms\SignInFormFactory */
+	private $signInFactory;
+	/** @var Forms\SignUpFormFactory */
+	private $signUpFactory;
+	public function __construct(Forms\SignInFormFactory $signInFactory, Forms\SignUpFormFactory $signUpFactory)
+	{
+		$this->signInFactory = $signInFactory;
+		$this->signUpFactory = $signUpFactory;
+	}
+
+	/**
+	 * Sign-in form factory.
+	 */
+	protected function createComponentSignInForm(): Form
+	{
+		return $this->signInFactory->create(function () {
+			$this->redirect('Homepage:');
+		});
+	}
+	/**
+	 * Sign-up form factory.
+	 */
+	protected function createComponentSignUpForm(): Form
+	{
+		return $this->signUpFactory->create(function () {
+			$this->redirect('Homepage:');
+		});
+	}
+	public function actionOut()
+	{
+		$this->getUser()->logout();
+	}
+}
